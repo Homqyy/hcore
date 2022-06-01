f905b9c0fe793474c53d108bb2a91cdca727f60d
homqyy <yilupiaoxuewhq@163.com>
2022-06-01 17:37:04 +0800

    更新 build_on_docker.sh 脚本

--- build_on_docker.sh
+++ build_on_docker.sh
@@ -10,6 +10,7 @@ DOCKER_FILE=$PROJECT_DIR/Dockerfile
 DOCKER_BUILD_LABEL="cn.homqyy.docker.build=hcore"
 WORK_DIR=/usr/src/hcore
 BUILD_TOOL=/usr/src/hcore/build.sh
+RANDOM_NAME="cn.homqyy.docker.build.hcore$( date +%Y%m%d%H%m%S )"
 
 ############################## Function
 
@@ -20,14 +21,14 @@ cd $PROJECT_DIR
 
 image_id=`docker images -qf "label=$DOCKER_BUILD_LABEL"`
 
-if ! image_id then
-    image_id=`docker build -q -f $DOCKER_FILE -t hcore.build.$( date +%Y%m%d%H%m%S%N ) --label $DOCKER_BUILD_LABEL $PROJECT_DIR`
+if [ -z "$image_id" ]; then
+    image_id=`docker build -q -f $DOCKER_FILE -t $RANDOM_NAME --label $DOCKER_BUILD_LABEL $PROJECT_DIR`
 fi
 
 container_id=`docker ps -qf "label=$DOCKER_BUILD_LABEL"`
 
-if container_id then
+if [ -n "$container_id" ]; then
     docker start $container_id
 else
-    docker run -v $PROJECT_DIR:$WORK_DIR -w $WORK_DIR $image_id $BUILD_TOOL
+    docker run -v $PROJECT_DIR:$WORK_DIR --name $RANDOM_NAME -w $WORK_DIR $image_id $BUILD_TOOL
 fi
